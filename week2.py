fd8948e5f0f8785e4614ff225301786922e933f6
soroutnikhil <138746374+soroutnikhil@users.noreply.github.com>
2023-07-06 16:06:59 +0530

    Add files via upload

---
+++ week2.py
@@ -0,0 +1,65 @@
+# Ans1 
+# We comment the code in 2 types 
+# my name is nikhil sorout
+"""my name nikhil sorout"""
+
+
+# Ans2
+# variables are used to store and manipulate data. 
+# like
+name="nikhil" #srting variable
+age="18"  #integer variable
+print(name,age)
+
+
+# Ans 3
+
+num = int(input("enter your number:"))
+a= str(num)     
+b= float(num)    
+c= bool(num)
+print("num_str:",a )
+print("num_float:",b )
+print("num_bool:",c )
+
+# Ans 4
+# Open vs code  and create a new file with a .py and srat code
+print("my name is nikhil")
+
+
+# Ans 5
+my_list=[1,2,3,4,5]
+sub_list = my_list[1:3]
+print(sub_list)
+
+# Ans 6
+a=8+9j
+print(type(a))
+
+
+# Ans7
+age=25
+print(age) #int variable
+
+
+
+# Ans 8 
+price=9.99
+print(price,type(price))
+
+# Ans9
+name ="nikhil sorout"
+print(name)
+
+
+# Ans 10
+a="hello World"
+b=a[6:11]
+print(b)
+
+
+# Ans 11
+is_student1=True
+is_student=False
+print(is_student1,is_student)
+
